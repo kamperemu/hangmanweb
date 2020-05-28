187ed0a29f1204de4dfea5459c5a794e69c0eb2e
kamperemu <34961229+kamperemu@users.noreply.github.com>
2020-05-28 16:58:55 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 from flask import Flask, render_template, request, redirect
+from game import gameUI
 
 app = Flask(__name__)
-
-
+guess = ""
+game = ""
+guessed=[]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/game')
+@app.route('/game', methods = ['GET','POST'])
 def game():
-    return render_template('game.html')
+    global guess
+    global game
+    if request.method == 'POST':
+        guess = request.form['letter']
+        return redirect('/game')
+    else:
+        game = gameUI(guess,guessed)
+        return render_template('game.html', game=game)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- game.py
+++ game.py
@@ -1,29 +1,26 @@
-def Convert(lst): 
-    res_dct = {lst[i]: lst[i + 1] for i in range(0, len(lst), 2)} 
-    return res_dct
+def guessUI(word,guessed):
+    guessUI=""
+    for i in word:
+        letter = False
+        for j in guessed:
+            if i==j:
+                letter = True
+        if letter:
+            guessUI+=i
+        else:
+            guessUI+="_"
+    return guessUI
+
+
+
 
 def split(word): 
     return [char for char in word] 
 
 
 
-word = str(input("Enter the word:"))
-
-word = split(word)
-
-guessed = []
-
-guess = str(input("Guess a letter: "))
-
-guessed.append(guess)
-
-for i in word:
-    letter = False
-    for j in guessed:
-        if i==j:
-            letter = True
-    if letter:
-        print(i, end = " ")
-    else:
-        print("_", end=" ")
-
+def gameUI(guess,guessed):
+    word="hello"
+    wordlist = split(word)
+    guessed.append(guess)
+    return guessUI(wordlist,guessed)
