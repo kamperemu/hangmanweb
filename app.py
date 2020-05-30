402b2172192aedac7c9a729872c6514f7779cf71
kamperemu <34961229+kamperemu@users.noreply.github.com>
2020-05-30 18:38:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
 from flask import Flask, render_template, request, redirect
-from game import gameUI
+from game import gameUI, reset
 
 app = Flask(__name__)
 guess = ""
 game = ""
-guessed=[]
+guessed = []
+word = ""
+noGuess = 0
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/game', methods = ['GET','POST'])
+
+@app.route('/word', methods=['GET', 'POST'])
+def word():
+    global word
+    global guessed
+    if request.method == 'POST':
+        word,guessed = reset()
+        word = request.form['word'].lower()
+        return redirect('/game')
+    else:
+        return render_template('word.html')
+        
+
+
+@app.route('/game', methods=['GET', 'POST'])
 def game():
+    global noGuess
+    global word
     global guess
     global game
+    global guessed
     if request.method == 'POST':
         guess = request.form['letter']
         return redirect('/game')
     else:
-        game = gameUI(guess,guessed)
-        return render_template('game.html', game=game)
+        game, noGuess = gameUI(word, guess, guessed, noGuess)
+        if game == word or noGuess>=8:
+            return render_template('winorlose.html', word=word, winorlose='WIN' if game == word else 'LOSE')
+            word,guessed = reset()
+        else:
+            return render_template('game.html', game=game, noGuess=noGuess)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- game.py
+++ game.py
@@ -1,26 +1,34 @@
-def guessUI(word,guessed):
-    guessUI=""
+# misc
+def split(word):
+    return [char for char in word]
+
+
+# function: shows how much progress the player has made
+def guessUI(word, guessed, noGuess):
+    guessUI = ""
+    noGuess = len([x for x in guessed if x not in word]) - 1
     for i in word:
         letter = False
         for j in guessed:
-            if i==j:
+            if i == j:
                 letter = True
         if letter:
-            guessUI+=i
+            guessUI += i
         else:
-            guessUI+="_"
-    return guessUI
+            guessUI += "_"
+    return guessUI, noGuess
 
 
-
-
-def split(word): 
-    return [char for char in word] 
-
-
-
-def gameUI(guess,guessed):
-    word="hello"
+# function: the game itself
+def gameUI(word, guess, guessed, noGuess):
     wordlist = split(word)
     guessed.append(guess)
-    return guessUI(wordlist,guessed)
+    game, noGuess = guessUI(wordlist, guessed, noGuess)
+    return game, noGuess
+
+
+# function: resets all variables
+def reset():
+    guessed = []
+    word = ""
+    return word, guessed
